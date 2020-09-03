d299d43b321a12aa2bed189b7af1d76f1270d1b4
Gabriel Havas <gh641617@ohio.edu>
2020-09-03 16:35:09 -0400

    Adding C++ program to the git-lab-2 repo

---
+++ git-lab-program.cc
@@ -0,0 +1,16 @@
+/*
+*   File: git-lab-program.cc
+*   Author: Gabriel Havas
+*   Date: 09/03/2020
+*   Description: C++ program in the git-lab-2 repository
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+    cout << "Hello Git!!" << endl;
+    return 0;
+} // main
